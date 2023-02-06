5bda609201b436ba07a18216b4bfe00dc596667c
kubotaissei <issei.k0504@me.com>
2023-02-06 17:02:08 +0000

    fix path

--- bin/download_and_preprocess.sh
+++ bin/download_and_preprocess.sh
@@ -6,4 +6,4 @@ docker run --rm -it \
     -v $HOME/.cache/:/root/.cache \
     --ipc=host \
     defamation:cuda11 \
-    python ./scripts/download_and_preprocess.py $1
+    cd root/workdir & python scripts/download_and_preprocess.py $0
